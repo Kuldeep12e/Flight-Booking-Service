e1669db95c8b413ab464c9c706489640cafc84a9
Kuldeep Yadav <kuldeep0105yadav@gmail.com>
2025-08-29 02:27:46 +0530

    Added booking model

---
+++ src/utils/common/enums.js
@@ -0,0 +1,18 @@
+const SEAT_TYPE = {
+    ECONOMY: 'economy',
+    BUSINESS: 'business',
+    FIRST_CLASS: 'first_class',
+    PREMIUM_ECONOMY: 'premium_economy'
+};
+
+const Booking_Status = {
+    BOOKED: 'booked',
+    CANCELLED: 'cancelled',
+    INITIATED: 'initiated',
+    PENDING: 'pending'
+};
+
+module.exports = {
+     SEAT_TYPE,
+     Booking_Status
+}; 
